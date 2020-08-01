0658b32b548e551161a8d9c9e78f4e8ea09ed2a1
Hacker-Ali <60498953+Hacker-Ali@users.noreply.github.com>
2020-08-01 11:51:52 +0530

    Update health_mananagement.py

--- health_mananagement.py
+++ health_mananagement.py
@@ -6,12 +6,12 @@ def intro():
 	print("\nWelcome to the Heath Management Program ! \nDeveloved by HAMMAD ALI ")
 	print("In this program you can record your diet and exercise and can also view them when required. ")
 	print("So lets get started!") 
-	print("************PLEASE DONT USE SPACES WHILE TYPING**************")
+	print("NOTE: YOUR INPUT IS NOT CASE SENSITIVE AND SPACES ARE ALSO NOT CONSIDERED\ni.e. ABC=abc=a b c")
 
 def getdatetime():
 	import datetime
 	a = datetime.datetime.now()
-	b = a.strftime('%d/%m/%y %H:%M:%S')
+	b = a.strftime('[%d/%m/%y %H:%M:%S]')
 	return b
 
 def update(x):
@@ -26,26 +26,36 @@ def fetch():
 		for line in file :
 			print(line , end="")
 
+def remove_spaces(str):
+	str1 = ""
+	for letter in str:
+		if letter == " ":
+			str1 += ""
+		else:
+			str1 += letter
+	return str1.lower()
+
+
 if __name__ == '__main__':
 
 	intro()
-	clientname = input("\nTYPE your name (ONLY FIRST NAME) : \n")
-	clientname = clientname.lower()
+	clientname = input("\nTYPE your name : \n")
+	clientname = remove_spaces(clientname)
 	previous_clients = []
 
 	while True:
 
 		feature = input(f"\n{clientname.upper()} TYPE on which you want to work (Diet or Exercise):\n")
-		feature = feature.lower()
+		feature = remove_spaces(feature)
 		feature_list = ["diet", "exercise"]
-		while feature.lower() not in feature_list:
+		while feature not in feature_list:
 			print(f"\n{clientname.upper()} choose correct feature \n")
 			feature= input(f"\n{clientname.upper()} TYPE on which you want to work (Diet or Exercise):\n")
 
 		def final():
 
 			task = input(f"\n{clientname.upper()} TYPE your option (Record or Fetch):\n")
-			task = task.lower()
+			task = remove_spaces(task)
 
 			if task == "record" :
 				done_what = input(f"\n{clientname.upper()} What you want to add to your record :\n")
@@ -63,7 +73,7 @@ if __name__ == '__main__':
 		final()
 
 		run_choice = input(f"{clientname.upper()} Do you want to run again (y/n)\n")
-		run_choice.lower()
+		run_choice = remove_spaces(run_choice)
 		if run_choice == "n" :
 			print(f"\nEnjoyed working with you {clientname.upper()}\n Bye!")
 			time.sleep_end()
@@ -71,5 +81,4 @@ if __name__ == '__main__':
 		elif run_choice == "y" :
 			continue
 		else :		
-			print(f"\n{clientname.upper()} Choose correct option\n") 
-			
+			print(f"\n{clientname.upper()} Choose correct option\n") 	
