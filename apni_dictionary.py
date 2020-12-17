5e0718fc9fd42c9a93a667a2fd7393e967aff800
Hacker-Ali <60498953+Hacker-Ali@users.noreply.github.com>
2020-12-17 11:17:19 +0530

    Delete apni_dictionary.py

--- apni_dictionary.py
+++
@@ -1,5 +0,0 @@
-d1 = { "mutable" : "changeable" , "immutable" : "unchangeble" , "screaming" : "shouting" , "gratitude" : "being thankful"}
-print("which meaning you want to search 1-mutable 2-immutable 3-screaming 4-gratitude :")
-search = input()
-result = d1[search]
-print( "meaning of" , search , "is" , result)
